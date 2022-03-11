c09d4a01c2fdd0ffedc488ebcb6a11c2229f3548
Mr. Hacker <greatestpythoncoder287@gmail.com>
2022-03-11 18:58:48 +0530

    other

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-#Import Library
+# Import Library
+import pickle
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn import linear_model
 import sklearn
-from sklearn.utils import shuffle
-import matplotlib.pyplot as plt
 from matplotlib import style
-import pickle
+from sklearn import linear_model
+from sklearn.utils import shuffle
 
 style.use("ggplot")
 
@@ -14,12 +15,13 @@ data = pd.read_csv("student-mat.csv", sep=";")
 
 predict = "G3"
 
-data = data[["G1", "G2", "absences","failures", "studytime","G3"]]
-data = shuffle(data) # Optional - shuffle the data
+data = data[["G1", "G2", "absences", "failures", "studytime", "G3"]]
+data = shuffle(data)  # Optional - shuffle the data
 
 x = np.array(data.drop([predict], 1))
-y =np.array(data[predict])
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    x, y, test_size=0.1)
 
 
 # TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
@@ -50,7 +52,7 @@ print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
 print("-------------------------")
 
-predicted= linear.predict(x_test)
+predicted = linear.predict(x_test)
 for x in range(len(predicted)):
     print(predicted[x], x_test[x], y_test[x])
 
